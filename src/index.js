65dba302c9913f7d1929733dfa354c8ce5396e49
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2020-08-09 17:51:44 +0300

    Done with Skills container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./styles/styles.scss";
 import "./styles/nav.scss";
 import "./styles/about.scss";
-
-const arr = ["asd", "dsa"];
-console.log([...arr, "sda"]);
+import "./styles/skills.scss";
+import "./styles/projects.scss";
