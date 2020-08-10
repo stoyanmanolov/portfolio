118d0795480f0ae2bc78e8edc2a18a8bc07f7256
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2020-08-10 17:03:07 +0300

    Done with styling for tablet screens

--- src/index.js
+++ src/index.js
@@ -9,38 +9,55 @@ import "./styles/skills.scss";
 import "./styles/projects.scss";
 import "./styles/footer.scss";
 import "./styles/drawer.scss";
+import "./js/navbar";
 
 // Toggle Drawer
 let open = false;
 
+function openDrawer() {
+  const togglerBars = document
+    .getElementById("toggler")
+    .getElementsByTagName("span");
+
+  Array.prototype.slice.call(togglerBars).forEach((toggleBar) => {
+    toggleBar.classList.add("open");
+  });
+
+  document.body.classList.add("drawer-body");
+  document.getElementById("drawer").classList.add("drawer-open");
+  document.getElementById("content").classList.add("drawer-content");
+  open = true;
+}
+
+function closeDrawer() {
+  const togglerBars = document
+    .getElementById("toggler")
+    .getElementsByTagName("span");
+
+  Array.prototype.slice.call(togglerBars).forEach((toggleBar) => {
+    toggleBar.classList.remove("open");
+  });
+
+  document.body.classList.remove("drawer-body");
+  document.getElementById("drawer").classList.remove("drawer-open");
+  document.getElementById("content").classList.remove("drawer-content");
+  open = false;
+}
+
 document.getElementById("toggler").addEventListener("click", (event) => {
   if (!open) {
-    const togglerBars = document
-      .getElementById("toggler")
-      .getElementsByTagName("span");
-
-    Array.prototype.slice.call(togglerBars).forEach((toggleBar) => {
-      toggleBar.classList.add("open");
-    });
-
-    document.body.classList.add("drawer-body");
-    document.getElementById("drawer").classList.add("drawer-open");
-    document.getElementById("content").classList.add("drawer-content");
-
-    open = true;
+    openDrawer();
   } else if (open) {
-    const togglerBars = document
-      .getElementById("toggler")
-      .getElementsByTagName("span");
-
-    Array.prototype.slice.call(togglerBars).forEach((toggleBar) => {
-      toggleBar.classList.remove("open");
-    });
-
-    document.body.classList.remove("drawer-body");
-    document.getElementById("drawer").classList.remove("drawer-open");
-    document.getElementById("content").classList.remove("drawer-content");
-
-    open = false;
+    closeDrawer();
   }
+});
+
+// Close the drawer when redirecting from the menu to a section.
+const menuItems = document.getElementById("drawer").getElementsByTagName("a");
+
+Array.prototype.slice.call(menuItems).forEach((menuItem) => {
+  console.log(menuItem);
+  menuItem.addEventListener("click", (event) => {
+    closeDrawer();
+  });
 });
---
+++ src/js/navbar.js
@@ -0,0 +1,12 @@
+const navBarItems = function () {
+  return `<li class="nav-item"><a href="#">Home</a></li>
+  <li class="nav-item"><a href="#header">About</a></li>
+  <li class="nav-item"><a href="#projects">Projects</a></li>
+  <li class="nav-item"><a href="#footer">Contacts</a></li>`;
+};
+
+const drawer = document.getElementById("drawer");
+drawer.innerHTML = navBarItems();
+
+const navBar = document.getElementById("navbar");
+navBar.innerHTML = navBarItems();
