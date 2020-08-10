eb27500b6607c11df92eb096114bba6a302c0b71
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2020-08-10 13:25:51 +0300

    Added a drawer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/brands";
-import "./styles/styles.scss";
+import "./styles/global/styles.scss";
 import "./styles/nav.scss";
 import "./styles/header.scss";
 import "./styles/skills.scss";
 import "./styles/projects.scss";
 import "./styles/footer.scss";
+import "./styles/drawer.scss";
+
+// Toggle Drawer
+let open = false;
+
+document.getElementById("toggler").addEventListener("click", (event) => {
+  if (!open) {
+    document.body.classList.add("drawer-body");
+    document.getElementById("drawer").classList.add("drawer");
+    document.getElementById("content").classList.add("drawer-content");
+    open = true;
+  } else if (open) {
+    document.body.classList.remove("drawer-body");
+    document.getElementById("drawer").classList.remove("drawer");
+    document.getElementById("content").classList.remove("drawer-content");
+    open = false;
+  }
+});
