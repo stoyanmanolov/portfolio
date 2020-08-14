b04bca6d78fc6f2eede367a3ee6fb85fa2f0410e
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2020-08-14 13:14:22 +0300

    Adding links to the projects

--- src/js/projects.js
+++ src/js/projects.js
@@ -2,16 +2,36 @@ const projects = [
   {
     name: "Shoes Seller",
     description: "A MERN Stack E-Commerce app",
+    demoLink: "",
+    githubLink: "https://github.com/stoyanmanolov/shoes-seller",
+    image: "src/images/ShoesSeller.jpg",
+  },
+  {
+    name: "Weather App",
+    description: "A weather app using OpenWeatherApi.",
+    demoLink: "https://weather-app-sm.netlify.app/",
+    githubLink: "https://github.com/stoyanmanolov/weather-app",
+    image: "src/images/WeatherApp.jpg",
+  },
+  {
+    name: "Typespeed",
+    description: "A typing speed measuring app.",
+    demoLink: "https://stoyanmanolov.github.io/typespeed/",
+    githubLink: "https://github.com/stoyanmanolov/typespeed",
+    image: "src/images/Typespeed.jpg",
   },
   {
     name: "Portfolio",
     description: "An HTML/CSS/JS portfolio website.",
+    demoLink: "http://stoyanmanolov.github.io/portfolio.",
+    githubLink: "https://github.com/stoyanmanolov/portfolio",
+    image: "src/images/Portfolio.jpg",
   },
 ];
 
 function sendProjectHTML(project, even) {
   return `<div class="card" data-aos=${even ? "fade-right" : "fade-left"}>
-    <div class="image"></div>
+    <img src="${project.image}" class="image" alt="Project"/>
     <div class="body">
       <div class='text'>
         <div class="card-heading">
@@ -20,9 +40,11 @@ function sendProjectHTML(project, even) {
         </div>
         <p>${project.description}</p>
       </div>
-      <div class="buttons">
-        <button class="btn demo">DEMO</button>
-        <button class="btn github">GITHUB</button>
+      <div class="links">
+        <a href="${project.demoLink}" class="btn demo" target="_blank">DEMO</a>
+        <a href="${
+          project.githubLink
+        }" class="btn github" target="_blank">GITHUB</a>
       </div>
     </div>
   </div>`;
